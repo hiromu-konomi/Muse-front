ae2345d3d2d0656dbf4894c44d53fc156e9d1901
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-11-24 10:01:54 +0900

    SideNavとSideSearchのコンポーネントを追加

---
+++ src/views/Main.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <SideNav />
+        <router-view />
+        <SideSearch />
+    </div>
+</template>
+
+<script>
+import SideNav from '../components/side/SideNav.vue'
+import SideSearch from '../components/side/SideSearch.vue'
+
+export default {
+    
+    components: {
+        SideNav,
+        SideSearch,
+    }
+}
+</script>
