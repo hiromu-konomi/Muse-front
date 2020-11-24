7a63bb7d5d28e8c8111e47eb57bfee865965a2eb
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-11-24 17:14:41 +0900

    router追加

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import Signup from '../views/Signup.vue'
+import Signin from "../views/Signin.vue"
 import Admin from '../views/Admin.vue'
+import Home from "../components/main/Home.vue"
+import Notification from "../components/main/Notification.vue"
+import Ranking from "../components/main/Ranking.vue"
+import Group from "../components/main/Group.vue"
+import Mypage from "../components/main/Mypage.vue"
+import Explore from "../components/main/Explore.vue"
+import Support from "../components/main/Support.vue"
 
 Vue.use(VueRouter)
 
@@ -18,9 +26,49 @@ const routes = [
     component: Signup
   },
   {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  },
+  {
     path: '/admin',
     name: 'Admin',
     component: Admin
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: Home
+  },
+  {
+    path: "/notification",
+    name: "notification",
+    component: Notification
+  },
+  {
+    path: "/ranking",
+    name: "ranking",
+    component: Ranking
+  },
+  {
+    path: "/group",
+    name: "group",
+    component: Group
+  },
+  {
+    path: "/mypage",
+    name: "mypage",
+    component: Mypage
+  },
+  {
+    path: "/explore",
+    name: "explore",
+    component: Explore
+  },
+  {
+    path: "/support",
+    name: "support",
+    component: Support
   },
 ]
 
