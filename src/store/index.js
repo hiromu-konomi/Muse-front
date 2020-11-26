478bfa5648ccac76e89fbe080d48ea84f5dd99b3
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-11-26 10:34:58 +0900

    store修正

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import firebase from "firebase";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {
-    sendEmail() {
-      const actionCodeSettings = {
-        url: "http://" + window.location.host + "/signin",
-      };
-      firebase.auth().languageCode = "ja";
-      const user = firebase.auth().currentUser;
-      user
-        .sendEmailVerification(actionCodeSettings)
-        .then(function() {
-          alert("認証メールを送信しました");
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
-  },
+  actions: {},
   modules: {},
 });
