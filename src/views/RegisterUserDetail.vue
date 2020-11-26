41bc30b0fd01912560c71ebafb2c4b5132ce533c
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-11-26 11:24:33 +0900

    ユーザー情報設定追加

---
+++ src/views/RegisterUserDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-layout row wrap justify-center>
+      <v-flex xs5 mt-5>
+        <v-card class="text-center">
+          <v-toolbar-title>ユーザー詳細設定</v-toolbar-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field> </v-text-field>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
