a39293fd1c980c0ff382082247c04fcf231e908b
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-11-30 17:17:27 +0900

    storeのモジュール化

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,13 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import reviewFrom from "./reviewForm.js";
 
-import Vue from 'vue'
-import Vuex from 'vuex'
-import { UPDATE_CURRENT } from "@/mutation-types";
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-
-  state: {
-    music_data: [],
-    current: null,
-  },
-  getters: {
-    current(state) {
-      return state.current;
-    },
-  },
-  mutations: {
-    [UPDATE_CURRENT](state, music) {
-      state.current = music;
-    },
-  },
-  actions: {
-    [UPDATE_CURRENT]({ commit }, music) {
-      commit(UPDATE_CURRENT, music);
-    },
-  },
+const store = new Vuex.Store({
   modules: {
-  }
-})
+    reviewFrom,
+  },
+});
+
+export default store;
