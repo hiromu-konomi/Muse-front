1d67c1855b6a3d75c9975aa896832c37ea640d4c
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-12-02 15:25:57 +0900

    v-mainの追加

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <SideNav />
-        <router-view />
         <SideSearch />
+        <v-main>
+            <router-view />
+        </v-main>
     </div>
 </template>
 
@@ -11,6 +13,7 @@ import SideNav from '../components/side/SideNav.vue'
 import SideSearch from '../components/side/SideSearch.vue'
 
 export default {
+    name: "Main",
     
     components: {
         SideNav,
