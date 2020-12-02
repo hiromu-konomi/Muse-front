1b831be504028ee15c90a64639149242354762a7
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-12-02 15:29:50 +0900

    template内をコメントアウト【Explore.vue】

--- src/components/main/Explore.vue
+++ src/components/main/Explore.vue
@@ -1,8 +1,7 @@
 <template>
-    <content>
         <v-row>
             <v-col>
-                <v-text-field
+                <!-- <v-text-field
                     class="search"
                     v-model="search_music_name"
                     type="text"
@@ -10,10 +9,9 @@
                     <template v-slot:append>
                         <v-icon type="button" @click="search_music">mdi-magnify</v-icon>
                     </template>
-                </v-text-field>
+                </v-text-field> -->
             </v-col>
         </v-row>
-    </content>
 </template>
 
 <script>
