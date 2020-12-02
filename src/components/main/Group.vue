b2dbeccd3693b28b6d254e6f1ce5ad7cf18fe983
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-12-02 15:28:47 +0900

    Cardを作成

---
+++ src/components/main/Group.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-container text-xs-center justify-center>
+        <v-layout justify-center>
+            <v-flex xs9 mt-5>
+                <v-card color="#385F73" dark>
+                    <v-card-text>グループ名lllllllllllllllllllllllllll</v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
