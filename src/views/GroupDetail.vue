88b2b6606fa7ad1c1efd4d5adbcc33ac2139dd46
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2020-12-03 16:58:20 +0900

    グループ詳細画面のViewを新規作成【GroupDetail.vue】

---
+++ src/views/GroupDetail.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <SideGroupDetail />
+        <v-main fluid>
+            <v-card
+                tile
+            >
+                <v-tabs
+                    v-model="tab"
+                    centered
+                    grow
+                >
+                    <v-tab
+                        v-for="item in items"
+                        :key="item.tab"
+                        :to="item.link"
+                    >
+                        {{ item.tab }}
+                    </v-tab>
+                </v-tabs>
+            </v-card>
+            <router-view />
+        </v-main>
+    </div>
+</template>
+
+<script>
+import SideGroupDetail from "../components/side/SideGroupDetail"
+
+export default {
+    components: {
+        SideGroupDetail,
+    },
+
+    data() {
+        return {
+            tab: null,
+            items: [
+                { tab: '情報', link: {name: "GroupInfo"}},
+                { tab: 'チャット', link: {name: "GroupChat"}},
+                { tab: 'メンバー', link: {name: "GroupMember"}},
+            ],
+        }
+    },
+}
+</script>
