bda1b707b4cf58e0b5ffc72b550b1615191a7abd
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-04 10:54:31 +0900

    SearchSong

--- src/views/SearchSong.vue
+++ src/views/SearchSong.vue
@@ -1,102 +1,105 @@
 <template>
-<v-app>
- <v-content>
- <v-form>
-<v-container>
-    <v-row>
-        <v-col>
-            <v-text-field
-              label="Search Song"
-               class="search"
-               v-model="search_music_name"
-               type="text">
-               <template v-slot:append>
-                <v-icon type="button" @click="search_music">mdi-magnify</v-icon>
-               </template>
-            </v-text-field>
-        </v-col>
-    </v-row>
-</v-container>
- </v-form>
-                <MusicInfo
-                  v-for="music of musics" :key="music.id"
-                  :linkable="true"
-                  :music="music"
-                  class="music"
-                  ></MusicInfo>
- </v-content>
-</v-app>
+  <v-app>
+    <v-content>
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-text-field
+                label="Search Song"
+                class="search"
+                v-model="search_music_name"
+                type="text"
+              >
+                <template v-slot:append>
+                  <v-icon type="button" @click="search_music"
+                    >mdi-magnify</v-icon
+                  >
+                </template>
+              </v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+      <MusicInfo
+        v-for="music of musics"
+        :key="music.id"
+        :linkable="true"
+        :music="music"
+        class="music"
+      ></MusicInfo>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import axios from 'axios'
-import MusicInfo from "../components/Info/MusicInfo.vue"
+import axios from "axios";
+import MusicInfo from "../components/Info/MusicInfo.vue";
 export default {
-    name: 'search',
+  name: "search",
 
-    components: {
-        MusicInfo
-    },
-    music_data: [],
-    data(){
-        return{
-            search_music_name: '',
-            searched_artist_name: "",
-            searched_picture: "",
-            searched_song: "",
-            headers: [
-                {
-                    text: 'picture',
-                    value: 'picture'
-                },
-                {
-                    text: 'artist',
-                    value: 'artist'
-                },
-                {
-                    text: 'song',
-                    value: 'song'
-                },
-            ],
-            musics: [],
-        };
-    },
-    methods:{
-        search_music: function () {
-            axios
-                .get("https://api.spotify.com/v1/search",{
-                headers: {
-                    "Authorization":
-                        "Bearer BQBj1LcRnoyo5ovkYdIV6pQESwJuThr3xTJ2cw7IwymeVohNxH9fbXZ5dzunkua2wEZ6gh7d7tFfvpCcQEY"
-                },
-                params: {
-                    q: this.search_music_name,
-                    limit: "50",
-                    offset: "0",
-                    type: "track",
-                    market: "JP"
-                }
-            })
-            .then(response=>{
-                this.music_data = response.data;
-                this.relative_data();
-                console.log(this.music_data)
-            })
-            .catch(error=> console.log(error));
+  components: {
+    MusicInfo,
+  },
+  music_data: [],
+  data() {
+    return {
+      search_music_name: "",
+      searched_artist_name: "",
+      searched_picture: "",
+      searched_song: "",
+      headers: [
+        {
+          text: "picture",
+          value: "picture",
         },
-        relative_data: function(){
-            this.musics = [];
+        {
+          text: "artist",
+          value: "artist",
+        },
+        {
+          text: "song",
+          value: "song",
+        },
+      ],
+      musics: [],
+    };
+  },
+  methods: {
+    search_music: function() {
+      axios
+        .get("https://api.spotify.com/v1/search", {
+          headers: {
+            Authorization:
+              "Bearer BQCY-C4HFbO7TJIfujM1DcFb0lDrFKc0nyneg9_PaxzuPSlzH8FClkk-dnRN62yBl10qPQ77Cpu1Zo38ddg",
+          },
+          params: {
+            q: this.search_music_name,
+            limit: "50",
+            offset: "0",
+            type: "track",
+            market: "JP",
+          },
+        })
+        .then((response) => {
+          this.music_data = response.data;
+          this.relative_data();
+          console.log(this.music_data);
+        })
+        .catch((error) => console.log(error));
+    },
+    relative_data: function() {
+      this.musics = [];
 
-            for(var i=0; i<this.music_data.tracks.items.length; i++){
-                this.musicInfo=this.music_data.tracks.items[i]
-                this.musics.push({
-                    searched_artist_name: this.musicInfo.artists[0].name,
-                    searched_song: this.musicInfo.name,
-                    searched_picture: this.musicInfo.album.images[2].url
-                })
-            }
-        }
-        },
-        }
+      for (var i = 0; i < this.music_data.tracks.items.length; i++) {
+        this.musicInfo = this.music_data.tracks.items[i];
+        this.musics.push({
+          searched_artist_name: this.musicInfo.artists[0].name,
+          searched_song: this.musicInfo.name,
+          searched_picture: this.musicInfo.album.images[2].url,
+        });
+      }
+    },
+  },
+};
 </script>
-
