9ea58fc8e7936f310f58e63a1701653781437056
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-07 15:02:18 +0900

    Firebase,DB保存処理

--- src/store/userDetail.js
+++ src/store/userDetail.js
@@ -1,14 +1,20 @@
 import axios from "axios";
+import firebase from "firebase";
 
 const userDetail = {
   state: {
-    userInformation: [],
+    userInformation: "",
+    userPhoto: null,
   },
   getters: {},
   mutations: {
     getUserDetail(state, userInformation) {
       console.log("getUserDetail = " + userInformation);
       state.userInformation = userInformation;
+    },
+    addUserPhoto(state, photo) {
+      console.log(photo);
+      state.photo = photo;
     },
   },
   actions: {
@@ -27,11 +33,48 @@ const userDetail = {
     },
 
     async addUserDetail({ commit }, user) {
+      console.log("storeに渡されたユーザー情報=" + user);
       await axios
         .post("http://localhost:8080/userDetail", user)
         .then(commit("getUserDetail", user))
         .catch((reason) => console.log(reason));
     },
+
+    addUserPhoto({ getters, commit }, photo) {
+      if (getters.uid) {
+        const uploadFile = photo;
+        const storageRef = firebase.storage().ref();
+
+        const uploadTask = storageRef.child(`images/${uploadFile.name}`);
+        uploadTask
+          .put(uploadFile)
+          .then((snapshot) => {
+            snapshot.ref
+              .getDownloadURL()
+              .then((downloadURL) => {
+                console.log("ダウンロードURL =" + downloadURL);
+                commit("addUserPhoto", downloadURL);
+              })
+              .catch((error) => {
+                console.log(error.message);
+              });
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      }
+    },
+    // addUserPhoto({ getters, commit }, photo) {
+    //   if (getters.uid) {
+    //     firebase
+    //       .firestore()
+    //       .collection(`users/${getters.uid}/userDetail`)
+    //       .add(photo)
+    //       .then((doc) => {
+    //         id: doc.id, photo;
+    //       });
+    //   }
+    // },
   },
 };
 
