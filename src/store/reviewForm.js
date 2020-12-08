b2b4c39cf75cbfadc5b166e5c9d9767e2c436c65
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-08 15:25:39 +0900

    reviewForm変更

--- src/store/reviewForm.js
+++ src/store/reviewForm.js
@@ -1,11 +1,17 @@
-import { UPDATE_CURRENT, UPDATE_USER_NAME } from "@/mutation-types";
+export const UPDATE_CURRENT = 'updateCurrent';
+export const UPDATE_USER_NAME = 'updateUserName';
+export const UPDATE_JENRE = 'updateUserJenre';
+export const UPDATE_REVIEW = 'updateUserReview';
 import axios from 'axios'
-
 const reviewForm = {
     state: {
         music_data: [],
         current: null,
-        userName: null,
+
+        jenre: null,
+        review: null,
+        userId: null,
+
     },
     getters: {
         current(state) {
@@ -20,6 +26,15 @@ const reviewForm = {
             state.userName = users;
             console.log("users=" + users);
         },
+        [UPDATE_JENRE](state, jenre) {
+            state.jenre = jenre;
+            console.log("jenre=" + jenre);
+        },
+        [UPDATE_REVIEW](state, review) {
+            state.review = review;
+            console.log("review=" + review);
+        },
+
     },
     actions: {
         [UPDATE_CURRENT]({ commit }, music) {
@@ -38,6 +53,30 @@ const reviewForm = {
             console.info("userName=" + this.userName)
             commit(UPDATE_USER_NAME, this.userName)
         },
+        postMusicInfo: async function(music) {
+            console.log("music=" + music)
+            await axios.post('http://localhost:8080/music',
+
+                    music,
+
+                )
+                // this.userName = res.data,
+                // this.jenre = res.data,
+                // this.review = res.data,
+                // commit(UPDATE_CURRENT, UPDATE_USER_NAME, UPDATE_JENRE, UPDATE_REVIEW, this.current, this.userName, this.jenre, this.review)
+        },
+        postFormInfo: async function(form) {
+            console.log("form=" + form)
+            await axios.post('http://localhost:8080/form',
+
+                    form,
+
+                )
+                // this.userName = res.data,
+                // this.jenre = res.data,
+                // this.review = res.data,
+                // commit(UPDATE_CURRENT, UPDATE_USER_NAME, UPDATE_JENRE, UPDATE_REVIEW, this.current, this.userName, this.jenre, this.review)
+        }
 
     },
 };
