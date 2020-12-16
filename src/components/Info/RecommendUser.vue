c722ae798c38cfad7d3afa03c467724346960546
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-16 13:22:30 +0900

    自分を表示に含めない

---
+++ src/components/Info/RecommendUser.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <!-- <div :class="{ linkable }" @click="onClick"> -->
+    <router-link
+      :to="{
+        name: 'userpage',
+        params: { user_id: user.userNum },
+      }"
+    >
+      <v-row class="userInfo">
+        <!-- <v-avatar @click="onClick">
+        <v-img :src="user.userPhoto"></v-img>
+      </v-avatar> -->
+        <div class="user">
+          {{ user.userName }}
+
+          {{ user.depId }}
+        </div>
+      </v-row>
+      <div class="profile">
+        {{ user.profile }}
+      </div>
+    </router-link>
+    <!-- </div> -->
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    user: { type: Object },
+    // linkable: { type: Boolean, default: false },
+  },
+
+  methods: {
+    onClick() {
+      // if (this.linkable) {
+      //   this.$router.push({
+      //     name: "userpage",
+      //     params: { user_id: user.userNum },
+      //   });
+      // }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user {
+  padding-top: 3px;
+  /* border: solid 1px black; */
+}
+
+.userInfo {
+  margin-left: 20px;
+  margin-top: 5px;
+}
+
+.profile {
+  /* margin-top: 5px; */
+  padding-bottom: 10px;
+}
+</style>
