2864995e1e13bcc9ad554366bfb4d0d81dbc0c33
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-12-21 17:11:04 +0900

    グループ通知がある時、ない時の処理

--- src/components/notification/GroupInviteNotification.vue
+++ src/components/notification/GroupInviteNotification.vue
@@ -1,10 +1,58 @@
 <template>
-  <h1>groupInvite</h1>
+  <div>
+    <v-list v-if="groupNoti.length">
+      <template v-for="(grpNoti, index) in groupNoti">
+        <v-list-item
+          :key="grpNoti.groupNotificationId"
+        >
+          <v-list-item-avatar>
+            <v-img src="https://cdn.vuetifyjs.com/images/lists/4.jpg"></v-img>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>
+              <h3>{{ grpNoti.groupName }}に招待されました</h3>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider :key="`second-${index}`"></v-divider>
+      </template>
+    </v-list>
+    <v-card v-else>
+      <v-card-text>
+        <h1>グループ招待はまだありません</h1>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios'
+import firebase from 'firebase'
 
+export default {
+  async created() {
+    await this.getGroupNoti();
+  },
+  data(){
+    return {
+      groupNoti: [],
+      userNum: undefined,
+    }
+  },
+  methods: {
+    async getGroupNoti(){
+      await firebase.auth().onAuthStateChanged((user) => {
+        this.userNum = user.uid;
+      });
+      const res = await axios.get('http://localhost:8080/groupNotification', {
+        params: {
+          receiverUserNum: this.userNum,
+        }
+      });
+      this.groupNoti = res.data.groupToResponseList;
+    }
+  }
 }
 </script>
 
