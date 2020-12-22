92aefd7fe7dea62c301ef8b3b0d21c3d4aca041b
澤口ありさ <sawaguchiarisa@sawaguchiarisanoMacBook-Pro.local>
2020-12-22 22:00:12 +0900

    エラー修正

--- src/store/index.js
+++ src/store/index.js
@@ -13,22 +13,20 @@ import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {
-    login_user: null,
-    userNum: null,
-  },
-  getters: {
-    uid: (state) => (state.login_user ? state.login_user.uid : null),
-    getUserbyUserNum: (state) => (id) =>
-      state.sUser.usersData.find((user) => user.userNum === id),
-    getUserPhotobyUserNum: (state) => (id) =>
-      state.uDetail.userPhoto.find((photo) => photo.userNum === id),
-    getFollowUserPost: (state) => (id) =>
-      state.fUser.myfollows_users.find((follow) => follow.userNum === id),
-  },
-  mutations: {
-    setLoginUser(state, user) {
-      state.login_user = user;
+    state: {
+        login_user: null,
+        userNum: null,
+    },
+    mutations: {
+        setLoginUser(state, user) {
+            state.login_user = user;
+        },
+        deleteLoginUser(state) {
+            state.login_user = null;
+        },
+        setUserId(state, userNum) {
+            state.userNum = userNum;
+        },
     },
     getters: {
         uid: (state) => (state.login_user ? state.login_user.uid : null),
@@ -38,28 +36,28 @@ const store = new Vuex.Store({
             state.uDetail.userPhoto.find((photo) => photo.userNum === id),
         getFollowUserPost: (state) => (id) =>
             state.fUser.myfollows_users.find((follow) => follow.userNum === id),
-    deleteLoginUser(state) {
-      state.login_user = null;
+        deleteLoginUser(state) {
+            state.login_user = null;
+        },
+        setUserId(state, userNum) {
+            state.userNum = userNum;
+        },
     },
-    setUserId(state, userNum) {
-      state.userNum = userNum;
+    actions: {
+        setLoginUser({ commit }, user) {
+            commit("setLoginUser", user);
+        },
+        logout() {
+            firebase.auth().signOut();
+        },
+        deleteLoginUser({ commit }) {
+            commit("deleteLoginUser");
+        },
+        setUserId({ commit }, userNum) {
+            commit("setUserId", userNum);
+        },
     },
-  },
-  actions: {
-    setLoginUser({ commit }, user) {
-      commit("setLoginUser", user);
-    },
-    logout() {
-     firebase.auth().signOut();
-    },
-    deleteLoginUser({ commit }) {
-      commit("deleteLoginUser");
-    },
-    setUserId({ commit }, userNum) {
-      commit("setUserId", userNum);
-    },
-  },
-  plugins: [createPersistedState({ // ストレージのキーを指定。デフォルトではvuex
+    plugins: [createPersistedState({ // ストレージのキーを指定。デフォルトではvuex
         key: 'reviewPost',
 
         // 管理対象のステートを指定。pathsを書かない時は`modules`に書いたモジュールに含まれるステート全て。`[]`の時はどれも保存されない
@@ -71,13 +69,14 @@ const store = new Vuex.Store({
         // ストレージの種類を指定する。デフォルトではローカルストレージ
         storage: window.sessionStorage
     })],
-  modules: {
-    rForm: reviewFrom,
-    uDetail: userDetail,
-    sUser: showUser,
-    fUser: followUser,
-    gDetail: groupDetail,
-  },
+
+    modules: {
+        rForm: reviewFrom,
+        uDetail: userDetail,
+        sUser: showUser,
+        fUser: followUser,
+        gDetail: groupDetail,
+    },
 });
 
 export default store;
