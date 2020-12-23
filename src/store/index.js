dfeb218a947f89b6133127a7ab9964608d4995df
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-23 17:55:24 +0900

    リロード問題解決

--- src/store/index.js
+++ src/store/index.js
@@ -6,77 +6,83 @@ import firebase from "firebase";
 import showUser from "./showUser.js";
 import followUser from "./followUser.js";
 import groupDetail from "./groupDetail.js";
-import createPersistedState from 'vuex-persistedstate'
+import createPersistedState from "vuex-persistedstate";
 
 // import userLogin from "./userLogin.js";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-    state: {
-        login_user: null,
-        userNum: null,
+  state: {
+    login_user: null,
+    userNum: null,
+  },
+  mutations: {
+    setLoginUser(state, user) {
+      state.login_user = user;
     },
-    mutations: {
-        setLoginUser(state, user) {
-            state.login_user = user;
-        },
-        deleteLoginUser(state) {
-            state.login_user = null;
-        },
-        setUserId(state, userNum) {
-            state.userNum = userNum;
-        },
+    deleteLoginUser(state) {
+      state.login_user = null;
     },
-    getters: {
-        uid: (state) => (state.login_user ? state.login_user.uid : null),
-        getUserbyUserNum: (state) => (id) =>
-            state.sUser.usersData.find((user) => user.userNum === id),
-        getUserPhotobyUserNum: (state) => (id) =>
-            state.uDetail.userPhoto.find((photo) => photo.userNum === id),
-        getFollowUserPost: (state) => (id) =>
-            state.fUser.myfollows_users.find((follow) => follow.userNum === id),
-        deleteLoginUser(state) {
-            state.login_user = null;
-        },
-        setUserId(state, userNum) {
-            state.userNum = userNum;
-        },
+    setUserId(state, userNum) {
+      state.userNum = userNum;
     },
-    actions: {
-        setLoginUser({ commit }, user) {
-            commit("setLoginUser", user);
-        },
-        logout() {
-            firebase.auth().signOut();
-        },
-        deleteLoginUser({ commit }) {
-            commit("deleteLoginUser");
-        },
-        setUserId({ commit }, userNum) {
-            commit("setUserId", userNum);
-        },
+  },
+  getters: {
+    uid: (state) => (state.login_user ? state.login_user.uid : null),
+    getUserbyUserNum: (state) => (id) =>
+      state.sUser.usersData.find((user) => user.userNum === id),
+    getUserPhotobyUserNum: (state) => (id) =>
+      state.uDetail.userPhoto.find((photo) => photo.userNum === id),
+    getFollowUserPost: (state) => (id) =>
+      state.fUser.myfollows_users.find((follow) => follow.userNum === id),
+    deleteLoginUser(state) {
+      state.login_user = null;
     },
-    plugins: [createPersistedState({ // ストレージのキーを指定。デフォルトではvuex
-        key: 'reviewPost',
+    setUserId(state, userNum) {
+      state.userNum = userNum;
+    },
+  },
+  actions: {
+    setLoginUser({ commit }, user) {
+      commit("setLoginUser", user);
+    },
+    logout() {
+      firebase.auth().signOut();
+    },
+    deleteLoginUser({ commit }) {
+      commit("deleteLoginUser");
+    },
+    setUserId({ commit }, userNum) {
+      commit("setUserId", userNum);
+    },
+  },
+  plugins: [
+    createPersistedState({
+      // ストレージのキーを指定。デフォルトではvuex
+      key: "reviewPost",
 
-        // 管理対象のステートを指定。pathsを書かない時は`modules`に書いたモジュールに含まれるステート全て。`[]`の時はどれも保存されない
-        paths: [
-            'rForm.current',
-            'uDetail.userInformation.userName',
-        ],
+      // 管理対象のステートを指定。pathsを書かない時は`modules`に書いたモジュールに含まれるステート全て。`[]`の時はどれも保存されない
+      paths: [
+        "rForm.current",
+        "uDetail.userInformation.userName",
+        "uDetail.userPhoto",
+        "sUser.usersData",
+        "uDetail.userInformation",
+      ],
 
-        // ストレージの種類を指定する。デフォルトではローカルストレージ
-        storage: window.sessionStorage
-    })],
+      // ストレージの種類を指定する。デフォルトではローカルストレージ
+      storage: window.sessionStorage,
+    }),
+  ],
 
-    modules: {
-        rForm: reviewFrom,
-        uDetail: userDetail,
-        sUser: showUser,
-        fUser: followUser,
-        gDetail: groupDetail,
-    },
+  modules: {
+    rForm: reviewFrom,
+    uDetail: userDetail,
+    sUser: showUser,
+    fUser: followUser,
+    gDetail: groupDetail,
+  },
 });
 
 export default store;
