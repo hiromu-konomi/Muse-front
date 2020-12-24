044a61f44fb3849e8e2f0a1a5453ab3517c44e43
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-24 16:43:10 +0900

    修正

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from "./router";
 import vuetify from "./plugins/vuetify";
 import firebase from "firebase";
 import store from "./store";
+
 // import Amplify, * as AmplifyModules from "aws-amplify";
 // import { AmplifyPlugin } from "aws-amplify-vue";
 // import aws_exports from "./aws-exports";
@@ -17,22 +18,22 @@ Vue.config.productionTip = false;
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyD_Ni-GZi8H6JFCKIDAdZFXvK3ibAcYjN4",
-    authDomain: "music-app-56cdc.firebaseapp.com",
-    databaseURL: "https://music-app-56cdc.firebaseio.com",
-    projectId: "music-app-56cdc",
-    storageBucket: "music-app-56cdc.appspot.com",
-    messagingSenderId: "791958197943",
-    appId: "1:791958197943:web:66a3a8e94145374aac1f32",
-    measurementId: "G-633CLDG4HQ",
+  apiKey: "AIzaSyD_Ni-GZi8H6JFCKIDAdZFXvK3ibAcYjN4",
+  authDomain: "music-app-56cdc.firebaseapp.com",
+  databaseURL: "https://music-app-56cdc.firebaseio.com",
+  projectId: "music-app-56cdc",
+  storageBucket: "music-app-56cdc.appspot.com",
+  messagingSenderId: "791958197943",
+  appId: "1:791958197943:web:66a3a8e94145374aac1f32",
+  measurementId: "G-633CLDG4HQ",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 new Vue({
-    router,
-    store,
-    vuetify,
-    render: (h) => h(App),
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
 }).$mount("#app");
