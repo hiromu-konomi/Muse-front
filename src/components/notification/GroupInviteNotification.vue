1c20e640b17a0b915dc81056f2641ef7138dc209
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2020-12-25 15:30:33 +0900

    パス変更

--- src/components/notification/GroupInviteNotification.vue
+++ src/components/notification/GroupInviteNotification.vue
@@ -2,9 +2,7 @@
   <div>
     <v-list v-if="groupNoti.length">
       <template v-for="(grpNoti, index) in groupNoti">
-        <v-list-item
-          :key="grpNoti.groupNotificationId"
-        >
+        <v-list-item :key="grpNoti.groupNotificationId">
           <v-list-item-avatar>
             <v-img src="https://cdn.vuetifyjs.com/images/lists/4.jpg"></v-img>
           </v-list-item-avatar>
@@ -25,50 +23,52 @@
       </template>
     </v-list>
     <v-list v-else>
-      <h1 style="color:#AAAAAA;">グループ招待はまだありません</h1>
+      <h1 style="color: #aaaaaa">グループ招待はまだありません</h1>
     </v-list>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-import firebase from 'firebase'
-import { mapActions } from 'vuex'
+import axios from "axios";
+import firebase from "firebase";
+import { mapActions } from "vuex";
 
 export default {
   async created() {
     await this.getGroupNoti();
   },
-  data(){
+  data() {
     return {
       groupNoti: [],
       userNum: undefined,
-    }
+    };
   },
   methods: {
-    async getGroupNoti(){
+    async getGroupNoti() {
       await firebase.auth().onAuthStateChanged((user) => {
         this.userNum = user.uid;
       });
-      const res = await axios.get('http://localhost:8080/groupNotification', {
-        params: {
-          receiverUserNum: this.userNum,
+      const res = await axios.get(
+        this.$store.state.baseUrl + "/groupNotification",
+        {
+          params: {
+            receiverUserNum: this.userNum,
+          },
         }
-      });
+      );
       this.groupNoti = res.data.groupToResponseList;
     },
     async toGroup(groupId) {
       await firebase.auth().onAuthStateChanged((user) => {
         this.userNum = user.uid;
       });
-    await this.setShowGroup({groupId: groupId, userNum: this.userNum});
+      await this.setShowGroup({ groupId: groupId, userNum: this.userNum });
       this.$router.push("/groupInfo");
     },
     ...mapActions(["setShowGroup"]),
-  }
-}
+  },
+};
 </script>
 
 <style>
-
 </style>
